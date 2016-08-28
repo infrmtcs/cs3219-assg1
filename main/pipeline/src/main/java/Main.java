4104e626c468e5bf17ead84367a6d3e82d77c856
infrmtcs <DuongDat.Informatics@rocketmail.com>
2016-08-28 15:08:38 +0800

    Choose architecture. Organize the Project Structure

---
+++ main/pipeline/src/main/java/Main.java
@@ -0,0 +1,8 @@
+/**
+ * Created by infrmtcs on 8/28/16.
+ */
+public class Main {
+    public static void main(String args[]) {
+        System.out.println("Pipeline");
+    }
+}
--- src/main/java/Main.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by infrmtcs on 8/27/16
- */
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-    }
-}
